208956976558d359f76528c140c9b2d6aad0db5f
alliespeer <alliespeer13@gmail.com>
2018-07-30 16:16:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # dog.rb
+
+class Dogs 
+  def bark 
+    
+    puts "Woof!"
+    
+  end
+  
+  def sit 
+    
+    puts "The Dog is sitting"
+    
+  end
+end
+    
+    
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
 # person.rb
+
+class Person 
+  
+  def talk
+    
+    puts "Hello World!"
+    
+  end
+  
+  def walk
+    
+    puts "The Person is walking"
+    
+  end
+end
